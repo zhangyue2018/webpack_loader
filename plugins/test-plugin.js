35be35f8c96ebeffa4155dd9bc7e818e80c58d5b
zhangyue <zhangyuea@hualala.com>
2023-02-03 19:58:18 +0800

    feat# 异步并行钩子：make

--- plugins/test-plugin.js
+++ plugins/test-plugin.js
@@ -18,32 +18,58 @@ class TestPlugin {
             console.log('TestPlugin environment');
         });
         // 由文档可知，emit是异步串行钩子
-        compiler.hooks.emit.tap('TestPlugin', async (compilation) => {
-            console.log('TestPlugin emit');
-            await new Promise(function(resolve, reject) {
-                setTimeout(() => {
-                    console.log('hahahahah');
-                    resolve();
-                }, 3000)
-            });
-        });
+        // compiler.hooks.emit.tap('TestPlugin', async (compilation) => {
+        //     console.log('TestPlugin emit');
+        //     await new Promise(function(resolve, reject) {
+        //         setTimeout(() => {
+        //             console.log('hahahahah');
+        //             resolve();
+        //         }, 3000)
+        //     });
+        // });
         
-        compiler.hooks.emit.tapAsync('TestPlugin', (compilation, callback) => {
-            console.log('aaaaaa');
+        // compiler.hooks.emit.tapAsync('TestPlugin', (compilation, callback) => {
+        //     console.log('aaaaaa');
+        //     setTimeout(() => {
+        //         console.log('TestPlugin emit ----111111-------');
+        //         callback();
+        //     }, 2000);
+        // });
+
+        // compiler.hooks.emit.tapPromise('TestPlugin', (compilation) => {
+        //     console.log('bbbbbb');
+        //     return new Promise(function(resolve, reject) {
+        //         setTimeout(() => {
+        //             console.log('TestPlugin emit --2222-----');
+        //             resolve();
+        //         }, 5000);
+        //     });
+        // });
+
+        // 由文档可知，make是异步并行钩子 AsyncParalleHook
+        // 注册的多个回调函数同步执行，回调函数里的异步任务，谁先完成谁先执行
+        compiler.hooks.make.tapAsync('TestPlugin', (compilation, callback) => {
+            console.log('aaaaa');
             setTimeout(() => {
-                console.log('TestPlugin emit ----111111-------');
+                console.log('TestPlugin make --11111-----');
                 callback();
-            }, 2000);
+            }, 3000);
         });
 
-        compiler.hooks.emit.tapPromise('TestPlugin', (compilation) => {
-            console.log('bbbbbb');
-            return new Promise(function(resolve, reject) {
-                setTimeout(() => {
-                    console.log('TestPlugin emit --2222-----');
-                    resolve();
-                }, 5000);
-            });
+        compiler.hooks.make.tapAsync('TestPlugin', (compilation, callback) => {
+            console.log('bbbbb');
+            setTimeout(() => {
+                console.log('TestPlugin make --22222-----');
+                callback();
+            }, 1000);
+        });
+
+        compiler.hooks.make.tapAsync('TestPlugin', (compilation, callback) => {
+            console.log('ccccc');
+            setTimeout(() => {
+                console.log('TestPlugin make --333333-----');
+                callback();
+            }, 2000);
         });
     }
 }
