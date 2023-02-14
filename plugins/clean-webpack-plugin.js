87a36c0f9a3b548497ad84595f694deb9fbf65e6
zhangyue <zhangyuea@hualala.com>
2023-02-14 18:06:56 +0800

    feat# 自定义插件CleanWebpackPlugin--done

--- plugins/clean-webpack-plugin.js
+++ plugins/clean-webpack-plugin.js
@@ -1,16 +1,13 @@
 class CleanWebpackPlugin {
     apply(compiler) {
+        // 2. 获取打包输出的目录
         const outputPath = compiler.options.output.path;
         const fs = compiler.outputFileSystem;
         // 1. 注册钩子， 在打包输出之前 emit
         compiler.hooks.emit.tap('CleanWebpackPlugin', (compilation) => {
-            // 2. 获取打包输出的目录
-
-
+            // 3. 通过fs删除打包输出的目录下的所有文件
+            this.removeFiles(fs, outputPath);
         });
-        // 3. 通过fs删除打包输出的目录下的所有文件
-        this.removeFiles(fs, outputPath);
-
     }
 
     removeFiles(fs, filePath) {
@@ -22,15 +19,19 @@ class CleanWebpackPlugin {
         files.forEach(file => {
             // 2.1 遍历所有文件，判断是文件夹还是文件
             const path = `${filePath}/${file}`;
-            const fileState = fs.statSync(path);
-            
-            if(fileState.isDirectory()) {
-                // 2.2 是文件夹就得删除下面所有文件，才能删除文件夹
-                this.removeFiles(fs, path);
-            } else {
-                // 2.2 是文件，就直接删除
-                fs.unlinkSync(path);
-            }
+            // 第一种方法
+            fs.rmdirSync(path, {recursive: true});
+
+            // 第二种方法
+            // const fileState = fs.statSync(path);
+            // if(fileState.isDirectory()) {
+            //     // 2.2 是文件夹就得删除下面所有文件，才能删除文件夹
+            //     this.removeFiles(fs, path);
+            //     fs.rmdirSync(path); // 刪除目录
+            // } else {
+            //     // 2.2 是文件，就直接删除
+            //     fs.unlinkSync(path);
+            // }
         });
 
     }
